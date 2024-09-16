e5c9eee80776f82e8a1a1bf206add5f02bc0e810
kndaniel123 <kndanielkariuki@gmail.com>
2024-09-16 21:16:49 +0300

    remove requisition form

--- upande_vf_custom/custom_scripts/server_scripts/delivery_note.py
+++ upande_vf_custom/custom_scripts/server_scripts/delivery_note.py
@@ -1,30 +1,102 @@
 import frappe
 
 def on_submit(doc, method):
-    create_sales_invoice(doc)
-
+    if doc.set_warehouse and doc.custom_driver_consignment_note_number:
+        create_sales_invoice(doc)
+        
+        variance_list =  variance_due_to_water_loss(doc)
+        
+        move_variance_to_wh(doc.custom_driver_consignment_note_number, doc.custom_variance_warehouse, doc.set_warehouse, doc.company, doc.cost_center, variance_list)
+        
 def create_sales_invoice(doc):
-    if doc.set_warehouse:
-        new_stck_entry = frappe.new_doc("Sales Invoice")
-        new_stck_entry.company = doc.get("company")
-        new_stck_entry.customer = doc.get("customer")
-        new_stck_entry.cost_center = doc.get("cost_center")
+    new_stck_entry = frappe.new_doc("Sales Invoice")
+    new_stck_entry.company = doc.get("company")
+    new_stck_entry.customer = doc.get("customer")
+    new_stck_entry.cost_center = doc.get("cost_center")
+    
+    for item in doc.items:
+        new_stck_entry.append("items", {
+            "item_code": item.get("item_code"),
+            "qty": item.get("qty"),
+            "item_name":item.get("item_name"),
+            "description":item.get("description"),
+            "cost_center": item.get("cost_center"),
+            "sales_order": item.get("against_sales_order"),
+            "so_detail": item.get("so_detail"),
+            "delivery_note": doc.get("name")
+        })
         
+    new_stck_entry.save()
+    
+    new_stck_entry.submit()
+        
+def get_dn_items(doc):
+    dn_items = []
+    if doc.custom_driver_consignment_note_number:
         for item in doc.items:
-            new_stck_entry.append("items", {
+            item_dict = {
                 "item_code": item.get("item_code"),
-                "qty": item.get("qty"),
-                "item_name":item.get("item_name"),
-                "description":item.get("description"),
-                "cost_center": item.get("cost_center"),
-                "sales_order": item.get("against_sales_order"),
-                "so_detail": item.get("so_detail"),
-                "delivery_note": doc.get("name")
-            })
+                "uom": item.get("uom"),
+                "qty": item.get("qty")
+            }
             
-        new_stck_entry.save()
+            if not item_dict in dn_items:
+                dn_items.append(item_dict)
+    
+    return dn_items
+
+def get_stock_entries(doc):
+    se_names = []
+    stck_entries = frappe.db.get_all("Stock Entry", filters={"custom_consignment_note": doc.custom_driver_consignment_note_number, "docstatus": 1}, fields=["name"])
+    
+    if stck_entries:
+        for entry in stck_entries:
+            if not entry.get("name") in se_names:
+                se_names.append(entry.get("name"))
+                
+    return se_names
+                
+def variance_due_to_water_loss(doc):
+    se_names = get_stock_entries(doc)
+    dn_items = get_dn_items(doc)
+    variance_list = []
+    
+    stck_items = frappe.db.get_all("Stock Entry Detail", filters={"parent": ['in', se_names]}, fields=["item_code", "qty", "uom"])
+    
+    for item in dn_items:
+        for stck in stck_items:
+            if item.get("item_code") == stck.get("item_code"):
+                if item.get("uom") == stck.get("uom"):
+                    variance = {
+                        "item_code": item.get("item_code"),
+                        "uom": item.get("uom"),
+                        "variance": float(stck.get("qty")) - float(item.get("qty"))
+                    }
+                    
+                    if not variance in variance_list:
+                        variance_list.append(variance)
+    return variance_list
+                        
+def move_variance_to_wh(c_no, variance_warehouse, s_warehouse, company, cost_center, variance_list):
+    new_stk_entry = frappe.new_doc("Stock Entry")
+    new_stk_entry.company = company
+    new_stk_entry.cost_center = cost_center
+    new_stk_entry.custom_consignment_note = c_no
+    new_stk_entry.from_warehouse = s_warehouse
+    new_stk_entry.to_warehouse = variance_warehouse
+    
+    for item in variance_list:
+        if float(item.get("variance")) > 0:
+            new_stk_entry.append("items",{
+                "item_code": item.get("item_code"),
+                "qty": item.get("variance"),
+                "uom": item.get("uom")
+            })
         
-        new_stck_entry.submit()
-        
-    else:
-        frappe.throw("Check Delivery Note Info!")
+    new_stk_entry.save()
+    new_stk_entry.submit()
+            
+    
+
+    
+    

cd4d842b47855493d06d8f350ce7d2d84376e6d5
codex-newton <mwanikinewton742@gmail.com>
2024-09-17 23:11:11 +0300

    latest changes after tests in prod

--- upande_vf_custom/custom_asset_scripts/custom_asset_value_adjustment.py
+++ upande_vf_custom/custom_asset_scripts/custom_asset_value_adjustment.py
@@ -1,156 +1,133 @@
-# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import flt, formatdate, get_link_to_form, getdate
-
+from frappe.utils import flt, formatdate, getdate
 from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
-    get_checks_for_pl_and_bs_accounts,
+	get_checks_for_pl_and_bs_accounts,
 )
 from erpnext.assets.doctype.asset.asset import get_asset_value_after_depreciation
-from erpnext.assets.doctype.asset_depreciation_schedule.asset_depreciation_schedule import (
-    make_new_active_asset_depr_schedules_and_cancel_current_ones,
-)
+from erpnext.assets.doctype.asset.depreciation import get_depreciation_accounts
+
 
 class CustomAssetValueAdjustment(Document):
-    def validate(self):
-        self.validate_date()
-        self.set_current_asset_value()
-        self.set_difference_amount()
+	def validate(self):
+		self.validate_date()
+		self.set_current_asset_value()
+		self.set_difference_amount()
 
-    def on_submit(self):
-        self.make_depreciation_entry()
-        self.update_asset(self.new_asset_value)
+	def on_submit(self):
+		self.make_depreciation_entry()
+		self.update_asset(self.new_asset_value)
 
-    def on_cancel(self):
-        self.update_asset(self.current_asset_value)
+	def on_cancel(self):
+		self.update_asset(self.current_asset_value)
 
-    def validate_date(self):
-        asset_purchase_date = frappe.db.get_value("Asset", self.asset, "purchase_date")
-        if getdate(self.date) < getdate(asset_purchase_date):
-            frappe.throw(
-                _("Asset Value Adjustment cannot be posted before Asset's purchase date <b>{0}</b>.").format(
-                    formatdate(asset_purchase_date)
-                ),
-                title=_("Incorrect Date"),
-            )
+	def validate_date(self):
+		# Fetch the asset purchase date to validate the adjustment date
+		asset_purchase_date = frappe.db.get_value("Asset", self.asset, "purchase_date")
+		if getdate(self.date) < getdate(asset_purchase_date):
+			frappe.throw(
+				_("Asset Value Adjustment cannot be posted before the Asset's purchase date <b>{0}</b>.").format(
+					formatdate(asset_purchase_date)
+				),
+				title=_("Incorrect Date"),
+			)
 
-    def set_difference_amount(self):
-        self.difference_amount = flt(self.current_asset_value - self.new_asset_value)
+	def set_difference_amount(self):
+		# Calculate the difference between the current and new asset values
+		self.difference_amount = flt(self.current_asset_value - self.new_asset_value)
 
-    def set_current_asset_value(self):
-        if not self.current_asset_value and self.asset:
-            self.current_asset_value = get_asset_value_after_depreciation(self.asset, self.finance_book)
+	def set_current_asset_value(self):
+		# Set the current asset value if not already set
+		if not self.current_asset_value and self.asset:
+			self.current_asset_value = get_asset_value_after_depreciation(self.asset, self.finance_book)
 
-    def make_depreciation_entry(self):
-        asset = frappe.get_doc("Asset", self.asset)
-        
-        # Fetch fixed asset account and revaluation account from the Asset Category's child table "Asset Category Account"
-        accounts = frappe.get_all(
-            "Asset Category Account",
-            filters={"parent": asset.asset_category}, 
-            fields=["fixed_asset_account", "custom_revaluation_account"]
-        )
-        
-        if not accounts:
-            frappe.throw(_("No accounts found for Asset Category {0}.").format(asset.asset_category))
-        
-        
-        fixed_asset_account = accounts[0].get("fixed_asset_account")
-        revaluation_account = accounts[0].get("custom_revaluation_account")
+	def make_depreciation_entry(self):
+		# Get the asset document
+		asset = frappe.get_doc("Asset", self.asset)
 
-        # Ensure both accounts were fetched correctly
-        if not fixed_asset_account:
-            frappe.throw(_("Fixed Asset Account not found in Asset Category {0}.").format(asset.asset_category))
+		# Fetch the accounts from the asset category child table
+		fixed_asset_account, custom_revaluation_account = frappe.db.get_value(
+			"Asset Category Account", 
+			{"parent": asset.asset_category, "company_name": asset.company}, 
+			["fixed_asset_account", "custom_revaluation_account"]
+		)
 
-        if not revaluation_account:
-            frappe.throw(_("Revaluation Account not found in Asset Category {0}.").format(asset.asset_category))
+		# Ensure accounts exist
+		if not fixed_asset_account or not custom_revaluation_account:
+			frappe.throw(_("Please ensure that both Fixed Asset Account and Custom Revaluation Account are set in the Asset Category for {0}.").format(asset.asset_category))
 
-        depreciation_cost_center, depreciation_series = frappe.get_cached_value(
-            "Company", asset.company, ["depreciation_cost_center", "series_for_depreciation_entry"]
-        )
+		# Fetch depreciation cost center and series from company settings
+		depreciation_cost_center, depreciation_series = frappe.get_cached_value(
+			"Company", asset.company, ["depreciation_cost_center", "series_for_depreciation_entry"]
+		)
 
-        je = frappe.new_doc("Journal Entry")
-        je.voucher_type = "Depreciation Entry"
-        je.naming_series = depreciation_series
-        je.posting_date = self.date
-        je.company = self.company
-        je.remark = f"Depreciation Entry against {self.asset} worth {self.difference_amount}"
-        je.finance_book = self.finance_book
+		# Create a new Journal Entry document
+		je = frappe.new_doc("Journal Entry")
+		je.voucher_type = "Depreciation Entry"
+		je.naming_series = depreciation_series
+		je.posting_date = self.date
+		je.company = self.company
+		je.remark = f"Depreciation Entry against {self.asset} worth {self.difference_amount}"
+		je.finance_book = self.finance_book
 
-        credit_entry = {
-            "account": fixed_asset_account,
-            "credit_in_account_currency": self.difference_amount,
-            "cost_center": depreciation_cost_center or self.cost_center,
-            "reference_type": "Asset",
-            "reference_name": asset.name,
-        }
+		# Create credit entry for Fixed Asset Account and debit entry for Custom Revaluation Account
+		credit_entry = {
+			"account": fixed_asset_account,
+			"credit_in_account_currency": self.difference_amount,
+			"cost_center": depreciation_cost_center or self.cost_center,
+			"reference_type": "Asset",
+			"reference_name": asset.name,
+		}
 
-        debit_entry = {
-            "account": revaluation_account,
-            "debit_in_account_currency": self.difference_amount,
-            "cost_center": depreciation_cost_center or self.cost_center,
-            "reference_type": "Asset",
-            "reference_name": asset.name,
-        }
+		debit_entry = {
+			"account": custom_revaluation_account,
+			"debit_in_account_currency": self.difference_amount,
+			"cost_center": depreciation_cost_center or self.cost_center,
+			"reference_type": "Asset",
+			"reference_name": asset.name,
+		}
 
-        accounting_dimensions = get_checks_for_pl_and_bs_accounts()
+		# Fetch any accounting dimensions that need to be applied
+		accounting_dimensions = get_checks_for_pl_and_bs_accounts()
 
-        for dimension in accounting_dimensions:
-            if dimension.get("mandatory_for_bs"):
-                credit_entry.update(
-                    {
-                        dimension["fieldname"]: self.get(dimension["fieldname"])
-                        or dimension.get("default_dimension")
-                    }
-                )
+		# Update accounting dimension data for both credit and debit entries
+		for dimension in accounting_dimensions:
+			if dimension.get("mandatory_for_bs"):
+				credit_entry.update(
+					{
+						dimension["fieldname"]: self.get(dimension["fieldname"])
+						or dimension.get("default_dimension")
+					}
+				)
 
-            if dimension.get("mandatory_for_pl"):
-                debit_entry.update(
-                    {
-                        dimension["fieldname"]: self.get(dimension["fieldname"])
-                        or dimension.get("default_dimension")
-                    }
-                )
+			if dimension.get("mandatory_for_pl"):
+				debit_entry.update(
+					{
+						dimension["fieldname"]: self.get(dimension["fieldname"])
+						or dimension.get("default_dimension")
+					}
+				)
 
-        je.append("accounts", credit_entry)
-        je.append("accounts", debit_entry)
+		# Append the entries to the Journal Entry document
+		je.append("accounts", credit_entry)
+		je.append("accounts", debit_entry)
 
-        je.flags.ignore_permissions = True
-        je.submit()
+		# Submit the Journal Entry
+		je.flags.ignore_permissions = True
+		je.submit()
 
-        self.db_set("journal_entry", je.name)
+		# Set the Journal Entry name in the Asset Value Adjustment document
+		self.db_set("journal_entry", je.name)
 
+	def update_asset(self, asset_value):
+		# Fetch the asset document
+		asset = frappe.get_doc("Asset", self.asset)
 
-    def update_asset(self, asset_value):
-        asset = frappe.get_doc("Asset", self.asset)
+		# Set the flag indicating a decrease in asset value due to value adjustment
+		asset.flags.decrease_in_asset_value_due_to_value_adjustment = True
 
-        if not asset.calculate_depreciation:
-            asset.value_after_depreciation = asset_value
-            asset.save()
-            return
-
-        asset.flags.decrease_in_asset_value_due_to_value_adjustment = True
-
-        if self.docstatus == 1:
-            notes = _(
-                "This schedule was created when Asset {0} was adjusted through Asset Value Adjustment {1}."
-            ).format(
-                get_link_to_form("Asset", asset.name),
-                get_link_to_form(self.get("doctype"), self.get("name")),
-            )
-        elif self.docstatus == 2:
-            notes = _(
-                "This schedule was created when Asset {0}'s Asset Value Adjustment {1} was cancelled."
-            ).format(
-                get_link_to_form("Asset", asset.name),
-                get_link_to_form(self.get("doctype"), self.get("name")),
-            )
-
-        make_new_active_asset_depr_schedules_and_cancel_current_ones(
-            asset, notes, value_after_depreciation=asset_value, ignore_booked_entry=True
-        )
-        asset.flags.ignore_validate_update_after_submit = True
-        asset.save()
+		# Prepare the asset for depreciation after the value adjustment
+		asset.prepare_depreciation_data(value_after_depreciation=asset_value, ignore_booked_entry=True)
+		asset.flags.ignore_validate_update_after_submit = True
+		asset.save()

e5c0e887e66e571489c52a8847ee74654449d5ed
kndaniel123 <kndanielkariuki@gmail.com>
2024-09-18 23:33:37 +0300

    add changes to requisition form

--- upande_vf_custom/upande_vf_custom/doctype/requisition_form/requisition_form.js
+++ upande_vf_custom/upande_vf_custom/doctype/requisition_form/requisition_form.js
@@ -3,7 +3,8 @@
 
 
 frappe.provide("erpnext.accounts.dimensions");
-erpnext.buying.setup_buying_controller();
+// erpnext.buying.setup_buying_controller();
+{% include 'erpnext/public/js/controllers/buying.js' %};
 
 frappe.ui.form.on('Requisition Form', {
 	setup: function(frm) {

30d6a8e4113333c0ff75859db13aa974d95d2386
kndaniel123 <kndanielkariuki@gmail.com>
2024-10-02 07:55:56 +0300

    comment HCF changes

--- upande_vf_custom/custom_scripts/client_scripts/stock_entry.js
+++ upande_vf_custom/custom_scripts/client_scripts/stock_entry.js
@@ -12,31 +12,12 @@ frappe.ui.form.on('Stock Entry', {
             callback: (r) => {
                 if(r.message){
                     data = r.message
-                    frm.doc.s_warehouse = data[0].s_warehouse
-                    frm.refresh_field("s_warehouse")
 
-                    processData(frm, data);
+                    frm.refresh_field("items")
+
                 }
                                
             }
         })
     }
 })
-
-function processData(frm, data) {
-    const childTableField = 'items';
-
-    const existing = new Set(frm.doc[childTableField].map(row => row.item_code)); 
-    data.forEach(d => {
-         if (!existing.has(d.item_code)) {
-            let newRow = frm.add_child(childTableField);
-            newRow.custom_hcf = d.custom_hcf; 
-            newRow.item_code = d.item_code;
-            newRow.s_warehouse = d.s_warehouse;
-
-            existing.add(data.item_code);
-        }
-    });
-    
-    frm.refresh_field(childTableField); 
-}
